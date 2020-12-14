2e242ed12b3564b7859271e240fdb40f199ab632
root <root@bjyz-sys-gpu-kongming6.bjyz.baidu.com>
2020-12-14 03:28:50 +0000

    fix fcos_loss

--- ppdet/modeling/loss/fcos_loss.py
+++ ppdet/modeling/loss/fcos_loss.py
@@ -1,27 +1,10 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-from paddle import fluid
-from IPython import embed
+import numpy as np
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
-from ppdet.modeling import ops
 from ppdet.core.workspace import register
 
 INF = 1e8
@@ -43,8 +26,18 @@ def flatten_tensor(inputs, channel_first=False):
             inputs, perm=[0, 2, 3, 1])
     else:
         input_channel_last = inputs
-    output_channel_last = paddle.flatten(input_channel_last, start_axis=0, stop_axis=2)
+    output_channel_last = paddle.flatten(input_channel_last, start_axis=0, stop_axis=2) # [N*H*W, C]
     return output_channel_last
+
+
+def sigmoid_cross_entropy_with_logits(input, label, ignore_index=-100, normalize=False):
+    output = F.binary_cross_entropy_with_logits(input, label, reduction='none')
+    mask_tensor = paddle.cast(label != ignore_index, 'float32')
+    output = paddle.multiply(output, mask_tensor)
+    if normalize:
+        sum_valid_mask = paddle.sum(mask_tensor)
+        output = output / sum_valid_mask
+    return output
 
 
 @register
@@ -57,7 +50,6 @@ class FCOSLoss(nn.Layer):
         iou_loss_type(str): location loss type, IoU/GIoU/LINEAR_IoU
         reg_weights(float): weight for location loss
     """
-
     def __init__(self,
                  loss_alpha=0.25,
                  loss_gamma=2.0,
@@ -80,32 +72,37 @@ class FCOSLoss(nn.Layer):
         Return:
             loss (Varialbes): location loss
         """
-        plw = paddle.multiply(pred[:, 0], positive_mask)
-        pth = paddle.multiply(pred[:, 1], positive_mask)
-        prw = paddle.multiply(pred[:, 2], positive_mask)
-        pbh = paddle.multiply(pred[:, 3], positive_mask)
-        tlw = paddle.multiply(targets[:, 0], positive_mask)
-        tth = paddle.multiply(targets[:, 1], positive_mask)
-        trw = paddle.multiply(targets[:, 2], positive_mask)
-        tbh = paddle.multiply(targets[:, 3], positive_mask)
+        plw = pred[:, 0] * positive_mask
+        pth = pred[:, 1] * positive_mask
+        prw = pred[:, 2] * positive_mask
+        pbh = pred[:, 3] * positive_mask
+
+        tlw = targets[:, 0] * positive_mask
+        tth = targets[:, 1] * positive_mask
+        trw = targets[:, 2] * positive_mask
+        tbh = targets[:, 3] * positive_mask
         tlw.stop_gradient = True
         trw.stop_gradient = True
         tth.stop_gradient = True
         tbh.stop_gradient = True
-        area_target = (tlw + trw) * (tth + tbh)
-        area_predict = (plw + prw) * (pth + pbh)
+
         ilw = paddle.minimum(plw, tlw)
         irw = paddle.minimum(prw, trw)
         ith = paddle.minimum(pth, tth)
         ibh = paddle.minimum(pbh, tbh)
+
         clw = paddle.maximum(plw, tlw)
         crw = paddle.maximum(prw, trw)
         cth = paddle.maximum(pth, tth)
         cbh = paddle.maximum(pbh, tbh)
+
+        area_predict = (plw + prw) * (pth + pbh)
+        area_target = (tlw + trw) * (tth + tbh)
         area_inter = (ilw + irw) * (ith + ibh)
         ious = (area_inter + 1.0) / (
             area_predict + area_target - area_inter + 1.0)
-        ious = paddle.multiply(ious, positive_mask)
+        ious = ious * positive_mask
+
         if self.iou_loss_type.lower() == "linear_iou":
             loss = 1.0 - ious
         elif self.iou_loss_type.lower() == "giou":
@@ -179,45 +176,39 @@ class FCOSLoss(nn.Layer):
         tag_bboxes_flatten.stop_gradient = True
         tag_center_flatten.stop_gradient = True
 
-        mask_positive = tag_labels_flatten > 0
-        mask_positive.stop_gradient = True
-        mask_positive_float = paddle.cast(mask_positive, dtype="float32")
+        mask_positive_bool = tag_labels_flatten > 0
+        mask_positive_bool.stop_gradient = True
+        mask_positive_float = paddle.cast(mask_positive_bool, dtype="float32")
         mask_positive_float.stop_gradient = True
+
         num_positive_fp32 = paddle.sum(mask_positive_float)
+        num_positive_fp32.stop_gradient = True
         num_positive_int32 = paddle.cast(num_positive_fp32, dtype="int32")
         num_positive_int32 = num_positive_int32 * 0 + 1
-        num_positive_fp32.stop_gradient = True
         num_positive_int32.stop_gradient = True
-        normalize_sum = paddle.sum(tag_center_flatten)
-        normalize_sum.stop_gradient = True
-        normalize_sum = paddle.sum(mask_positive_float * normalize_sum)
+
+        normalize_sum = paddle.sum(tag_center_flatten * mask_positive_float)
         normalize_sum.stop_gradient = True
 
         # expand_onehot_labels
-        '''
-        label_channels = cls_logits_flatten.shape[-1]
-        tag_labels_bin = np.zeros(shape=(tag_labels_flatten.shape[0], label_channels)) # tag_labels_flatten.new_full((tag_labels_flatten.shape[0], label_channels), 0)
-        #inds = np.nonzero((tag_labels_flatten >= 0) & (tag_labels_flatten < label_channels), as_tuple=False).squeeze()
-        inds = np.nonzero((tag_labels_flatten > 0)).squeeze()[:, 0]
-        if inds.numel() > 0:
-            tag_labels_bin[inds, tag_labels_flatten[inds]] = 1
-        # embed()
+        categories = cls_logits_flatten.shape[-1]
+        tag_labels_bin = np.zeros(shape=(tag_labels_flatten.shape[0], categories))
+        tag_labels_flatten_b = paddle.squeeze(tag_labels_flatten, axis=-1).numpy()
+        inds = np.nonzero((tag_labels_flatten_b > 0) & (tag_labels_flatten_b <= categories))[0]#.squeeze()
+        if len(inds) > 0:
+            tag_labels_bin[inds, tag_labels_flatten_b[inds]-1] = 1
+        tag_labels_flatten_bin = paddle.to_tensor(tag_labels_bin.astype('float32'))
+        cls_loss = F.sigmoid_focal_loss(cls_logits_flatten, tag_labels_flatten_bin)/num_positive_fp32
 
-        tag_labels_flatten_bin = paddle.to_tensor(tag_labels_bin.astype('float32'),
-        cls_loss = F.sigmoid_focal_loss(cls_logits_flatten, tag_labels_flatten_bin)
-                    #, normalizer=paddle.sum(num_positive_int32))# reduction='mean')
-        '''
-        cls_loss = fluid.layers.sigmoid_focal_loss(cls_logits_flatten, tag_labels_flatten, num_positive_int32) / num_positive_fp32
-        #cls_loss = F.sigmoid_focal_loss(cls_logits_flatten, tag_labels_flatten, num_positive_int32) / num_positive_fp32
+        mask_positive_float = paddle.squeeze(mask_positive_float, axis=-1)
+        tag_center_flatten = paddle.squeeze(tag_center_flatten, axis=-1)
+        reg_loss = self.__iou_loss(bboxes_reg_flatten, tag_bboxes_flatten,
+            mask_positive_float, weights=tag_center_flatten)
+        reg_loss = reg_loss * mask_positive_float / normalize_sum
 
-        reg_loss = self.__iou_loss(bboxes_reg_flatten, tag_bboxes_flatten, mask_positive_float, tag_center_flatten)
-        reg_loss = paddle.multiply(reg_loss, mask_positive_float) / normalize_sum
-
-        #ctn_loss = fluid.layers.sigmoid_cross_entropy_with_logits(
-        #    x=centerness_flatten,
-        #    label=tag_center_flatten) * mask_positive_float / num_positive_fp32
-        ctn_loss = ops.sigmoid_cross_entropy_with_logits(centerness_flatten, tag_center_flatten, ignore_index=-1, normalize=True)
-        ctn_loss = paddle.multiply(ctn_loss, mask_positive_float) / normalize_sum
+        centerness_flatten = paddle.squeeze(centerness_flatten, axis=-1)
+        ctn_loss = sigmoid_cross_entropy_with_logits(centerness_flatten, tag_center_flatten) / num_positive_fp32
+        ctn_loss = ctn_loss * mask_positive_float / normalize_sum
 
         loss_all = {
             "loss_centerness": paddle.sum(ctn_loss),
